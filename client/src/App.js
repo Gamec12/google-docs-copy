93fad4f0791b24f2d987b542d9d9615b3d9ccced
Adam Loay <adamlo2ay@gmail.com>
2023-08-08 18:45:24 +0300

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,22 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  return
-  <Router>
-    <TextEditor />
-  </Router>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact> // each route is a case on our switch
+          <Redirect to={`/documents/ ${uuidv4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+
+    </Router>
+  )
 }
 
 export default App;
